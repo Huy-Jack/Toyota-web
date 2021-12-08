60ddd1d61a2e2680db171bf5a9a1a15eab14910f
DangPhuc141101 <dangphucff@gmail.com>
2021-12-08 11:31:02 +0700

    init-basic

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+
+mongoose.connect('mongodb://localhost:27017/toyota', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, 'connection error:'));
+db.once("open", () => {
+    console.log('Database connected');
+})
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+// using ejs
+app.get('/home', (req, res) => {
+    const person = {
+        name: 'Wave',
+        age: 20
+    }
+    res.render('home', {person});
+})
+
+app.get('/greeting', (req, res) =>{
+    res.send('Hello my frined!');
+})
+
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT 3000!!!');
+})
