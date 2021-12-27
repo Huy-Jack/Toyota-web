8eb31fa4b135f3692aa74ac6134626bd3cd0c073
DangPhuc141101 <dangphucff@gmail.com>
2021-12-27 15:57:48 +0700

    router-products

--- src/routes/all.js
+++ src/routes/all.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routes = express.Router();
-
+const Car = require('../models/cars');
 
 routes.get('/', (req, res) => {
-    const person = {
-        name: 'Wave',
-        age: 20
-    }
-    res.render('home', { person });
+    res.render('home');
+})
+
+routes.get('/products', async (req, res) => {
+    const car = await Car.find({});
+    res.render('products', {car});
 })
 
 module.exports = routes;
